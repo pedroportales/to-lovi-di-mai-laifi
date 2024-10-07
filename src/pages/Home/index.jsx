fc656f7229b50c8a8078de02a65a3240ddf666bd
Pedro Portales <pedroportales@riseup.net>
2024-10-06 23:47:38 -0300

    init with bootstrap

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,64 @@
 import './style.css'
-import Galeria from '../../components/Galeria'
+
 
 function Home() {
-    const imagens = [
-        {url: "https://www.arqplace.com.br/teste-teste", alt: "Imagem 1"}
-    ]
 
     return (
-        <div>
-            <h1>Olá, amor!</h1>
-            <Galeria imagens={imagens} />
-        </div>
+        <>
+            <div>
+                <h1>Olá, amor!</h1>
+                <p>Feliz primeiro mês!! Que venham muitos mais pela frente...</p>
+                <br />
+            </div>
+            <div id="carouselExample" className="carousel slide">
+                <div className="carousel-indicators">
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide-to={0}
+                        className="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                    />
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleIndicators"
+                        data-bs-slide-to={1}
+                        aria-label="Slide 2"
+                    />
+                </div>
+
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="src/assets/photo_2024-09-29_10-15-05.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="src/assets/photo_2024-09-29_10-15-51.jpg" className="d-block w-100" alt="..." />
+                    </div>
+
+                </div>
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExample"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon" aria-hidden="true" />
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExample"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon" aria-hidden="true" />
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+
+            
+        </>
     )
 }
 
