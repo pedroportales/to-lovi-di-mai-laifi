d3efa0030cd388b43cdc4fefe3828bcbe2890460
Pedro Portales <pedroportales@riseup.net>
2024-10-07 23:52:02 -0300

    the end

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,11 +6,11 @@ function Home() {
     return (
         <>
             <header>
-                    <h1>Olá, amor!! ❤🌹</h1>
+                    <h1>Feliz 1 mês, meu amor!! ❤️🌹</h1>
             </header>
             <br />
             <main>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, libero. Mollitia ea amet tempore rerum? Doloremque dolore officia repudiandae labore, consequatur saepe, suscipit eaque non perferendis architecto delectus, fugiat soluta. Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore cum corporis nihil consequuntur laudantium culpa, perferendis ut. Reprehenderit commodi explicabo, a, ea ullam eum rerum exercitationem aliquam dolorum, provident sequi.</p>
+                <p>Eu sei que esse não é o melhor presente do mundo KKKKK Mas eu fiz de coração pra ti!! E gostaria de lhe agradecer por ser essa garota tão especial que tem sido uma benção tão grande em minha vida... Abaixo separei 4 fotos de nós dois, um ao lado do outro, e em todas podemos encontrar nada mais, nada menos que o sorriso mais belo de toda existência: </p>
                 <div id="carouselExample" className="carousel slide">
                     <div className="carousel-indicators">
                         <button
@@ -39,29 +39,21 @@ function Home() {
                             data-bs-slide-to={3}
                             aria-label="Slide 4"
                         />
-                        <button
-                            type="button"
-                            data-bs-target="#carouselExampleIndicators"
-                            data-bs-slide-to={4}
-                            aria-label="Slide 5"
-                        />
                     </div>
 
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <img src="/to-lovi-di-mai-laifi/photo_2024-09-29_10-15-05.jpg" className="d-block w-100" alt="..." />
                         </div>
+                        
+                        <div className="carousel-item">
+                            <img src="/to-lovi-di-mai-laifi/photo_2024-09-29_10-15-13.jpg" className='d-block w-100' alt="..." />
+                        </div>
                         <div className="carousel-item">
                             <img src="/to-lovi-di-mai-laifi/photo_2024-09-29_10-15-51.jpg" className="d-block w-100" alt="..." />
                         </div>
                         <div className="carousel-item">
                             <img src="/to-lovi-di-mai-laifi/photo_2024-10-04_09-21-54.jpg" className="d-block w-100" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src="/to-lovi-di-mai-laifi/photo_2024-10-02_19-40-21.jpg" className='d-block w-100' alt="..." />
-                        </div>
-                        <div className='carousel-item'>
-                            <img src="/to-lovi-di-mai-laifi/photo_2024-09-30_13-38-12.jpg" className='d-block w-100' alt="..." />
                         </div>
                     </div>
                     <button
@@ -84,7 +76,15 @@ function Home() {
                     </button>
                 </div>
                 <br />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores dignissimos est cumque! Modi perferendis quasi culpa voluptate, officia quos labore facilis voluptatum architecto deserunt nulla ab porro. Iure, eveniet veniam. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, facilis necessitatibus eveniet minima voluptatem est voluptatibus quo. Dolor ullam repudiandae earum, omnis saepe, sint, et cupiditate illo dolore consequatur fugit.</p>
+                <p>Você me conquistou pelo seu jeito doce e incrível de ser!! E eu fico aqui tentando te descrever, tentando te elogiar... Mas me faltam palavras para expressar!! É como disse o Jorge Vercillo: "Ela une todas as coisas... Como eu poderia explicar?... Um doce mistério de rio... Com a transparência de um mar"</p>
+                <img src="/to-lovi-di-mai-laifi/photo_2024-09-29_10-15-19.jpg" alt="" className="img-out" />
+                <p>Estou ansioso para que possamos entrar juntos no Templo!! Se for parar pra pensar, ainda não fizemos isso, mesmo durante o período de Casa Aberta. Isso quer dizer que quando fizermos, será literalmente a primeira vez, e será única!!</p>
+                <img src="/to-lovi-di-mai-laifi/photo_2024-09-30_13-38-12.jpg" alt="..." className="img-out" />
+                <p>Também estou esperançoso de lhe carregar em meus braços, você com um belo vestido branco (que vai ficar ainda mais belo em você), e tendo juntos esse momento tão feliz e especial que nos é ordenado pelo Pai! Muito obrigado por ter ✨guardado✨ meu coração contigo, meu amor!! Pode ficar com ele (sem prazo de devolução 😝). Que venhamos muitos anos mais, e uma eternidade!! ❤️✨</p>
+                <img src="/to-lovi-di-mai-laifi/photo_2024-10-02_19-40-21.jpg" alt="" className="img-out" />
+                <br /><br /><br />
+                <h1>Eu te amo!! E vou gritar pra todo mundo ouvir!! ❤️❤️❤️🫂</h1>
+                <br /><br /><br />
             </main>
         </>
     )
